53d84d4a26e0d22562910b64f1a28e04316b9595
ajay14022003 <152485599+ajay14022003@users.noreply.github.com>
2024-05-22 12:25:42 +0530

    Add files via upload

---
+++ Fontend/Book-Application/src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Header from "./Components/Header";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Components/Home";
+import AddBook from "./Components/AddBook";
+import Books from "./Components/Book/Books";
+import About from "./Components/About";
+import BookDetail from "./Components/Book/BookDetail";
+function App() {
+  return (
+    <React.Fragment>
+      <header>
+        <Header />
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} exact />
+          <Route path="/add" element={<AddBook />} exact />
+          <Route path="/books" element={<Books />} exact />
+          <Route path="/about" element={<About />} exact />
+          <Route path="/books/:id" element={<BookDetail />} exact />
+        </Routes>
+      </main>
+    </React.Fragment>
+  );
+}
+
+export default App;
